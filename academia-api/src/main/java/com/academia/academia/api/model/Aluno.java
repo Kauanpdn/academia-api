3a3961cd7bf847ade0070313215b3f3772d2edcd
OkauanDev <teko4278@gmail.com>
2026-02-08 01:24:10 -0300

    começando e estudando sobre o Spring Boot

---
+++ academia-api/src/main/java/com/academia/academia/api/model/Aluno.java
@@ -0,0 +1,73 @@
+package com.academia.academia.api.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "aluno")
+
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    
+    private String email;
+
+    private String telefone;
+
+    @Column(name = "dataNascimento")
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Aluno(){}
+
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+    
+}
